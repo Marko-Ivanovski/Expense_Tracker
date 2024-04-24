7f74d53d6ecfa09896e7a268da7d7878778be3bd
Marko-Ivanovski <markoivanovski.mi@icloud.com>
2024-04-25 00:12:28 +0200

    Final commit

--- ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+083dda4b749f605aa91e754393568f280d50505f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
